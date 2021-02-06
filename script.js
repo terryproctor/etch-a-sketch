dc68e815e01b9f60775c3db9dc08d817ac038472
terryproctor <terryproctor2015@outlook.com>
2021-02-06 22:44:34 +0000

    etch a sketch mark1 - functional

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('start')
 
 boxAmount = 169;
 
+//create a board
 for(x=0; x<=boxAmount;x++) { 
   var board = document.createElement('div');
     board.className = "box";
@@ -13,6 +14,7 @@ var mouseTarget = document.getElementsByClassName('box');
 document.querySelectorAll('.box').forEach(item => {
     item.addEventListener('mouseout', event =>
     {
-        
+        /*console.log('test')*/
+        item.style.backgroundColor = 'red'
     })
 })
